9722ac9dcff7664b14c178e135c80cc942c57db4
Ivan Volkovets <batmad857@gmail.com>
2022-07-17 17:54:18 +0300

    init(TRUCK-STORE-1):initialized project, installed dependencies, configured webpack, added source files

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const json = require('./dataset.json');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+function cuka(a){
+    let result = JSON.stringify(a);
+    let res = JSON.parse(result)
+    console.log(res);
+    return res
+}
+let f = cuka(json);
+
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'img/[name].[ext]'
+    
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        template:  path.resolve(__dirname, 'src', 'index.html'),
+        templateParameters: f,
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: `src/images`, to: `images` },
+        ],
+      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }  
+    ]
+  }
+}
